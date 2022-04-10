9948272d1701c5bf0c709a2f400a57b918db5038
Jahid <jahidhasan.developer@gmail.com>
2022-04-10 13:54:32 +0600

    done

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-function menuToggle(){
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
+function menuToggle() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").classList.add('active');
+  } else {
+    document.getElementById("header").classList.remove('active')
+  }
 }
